586d9645a1bf1638cff66f81b9aa3f5ff098807d
Chaumette <mathischaumette.dev@gmail.com>
2021-04-12 13:18:19 +0200

    ajout exo11

---
+++ exo_11.rb
@@ -0,0 +1,15 @@
+puts "Quel âge tu as ?"
+
+print ">"
+
+age = gets.chomp.to_i
+i = 0
+
+    
+while age >= 0 
+    puts "Il y a " + age.to_s + " ans" + " tu avais : " + i.to_s + " ans"
+    age -= 1
+    i += 1 
+end
+
+
