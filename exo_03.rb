4849c3b7f46c766a59b3f17b389da6b07f6262d8
Chaumette <mathischaumette.dev@gmail.com>
2021-04-12 10:07:14 +0200

    Ajout exo3

---
+++ exo_03.rb
@@ -0,0 +1,11 @@
+puts "Quelle est ton année de naissance ? Pour savoir quel âge tu avais en 2017."
+
+print ">"
+
+years = gets.chomp
+
+print "Tu avais : " 
+
+print 2017 - years.to_i 
+print " ans en 2017 "
+
