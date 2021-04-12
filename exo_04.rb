8639d379d5d6e07f9138379f90406f85e12e94ce
Chaumette <mathischaumette.dev@gmail.com>
2021-04-12 10:11:53 +0200

    ajout exo4

---
+++ exo_04.rb
@@ -0,0 +1,11 @@
+puts "Quelle est ton année de naissance ? Pour savoir quelle sera l'année de tes 100 ans ?"
+
+print ">"
+
+years = gets.chomp
+
+print "Tu auras 100 ans en : "
+
+print years.to_i + 100
+
+print " !"
